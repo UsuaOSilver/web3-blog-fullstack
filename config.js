35f50f8e138b6c835fcbb2ac703a0d675b27ee74
napee19 <napee1904@gmail.com>
2022-07-07 18:23:22 -0700

    theGraph

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
   export const contractAddress = "0x40690AFcA8C5647aD8Ee6cB1E42ccAC0fAbb3346"
-  export const ownerAddress = "0xDc62552271EfE7E388D12D79990E83fdFb1E8b70
+  export const ownerAddress = "0xDc62552271EfE7E388D12D79990E83fdFb1E8b70"
   
