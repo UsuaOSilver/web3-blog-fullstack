86c4d40e4f7757cb18bdcad045978f7f63e1ca7f
napee19 <napee1904@gmail.com>
2022-07-07 17:58:40 -0700

    Deploy to Mumbai successfully

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  export const contractAddress = "0x40690AFcA8C5647aD8Ee6cB1E42ccAC0fAbb3346"
+  export const ownerAddress = "0xDc62552271EfE7E388D12D79990E83fdFb1E8b70
   
--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
      mumbai: {
        url: "https://polygon-mumbai.infura.io/v3/a55d24261c534a599245e3eb82e6ad0b",
-       accounts: [`${process.env.PRIVATE_KEY}`]
+       accounts: ["0x2caf42750094f366a1a99467a05fee28ce498c30c8a3d57e000de04bde2bcd37"]
      },
     // polygon: {
     //   url: "https://polygon-mumbai.infura.io/v3/a55d24261c534a599245e3eb82e6ad0b",
